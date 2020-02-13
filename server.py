f6d2173f0205ee1c136cb65dc97ff75fcd1e453b
Pepe García <pepe@pepegar.com>
2020-02-13 01:06:48 +0100

    add heroku shenanigans

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "hello!"
+
+port = "5000"
+
+if "PORT" in os.environ:
+    port = os.environ["PORT"]
+
+app.run(port=port, host="0.0.0.0")
